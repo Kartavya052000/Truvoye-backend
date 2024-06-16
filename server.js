251df7f5ac1f00fe7d5a42ca21dffc63936a40ce
Kartavya Bhayana <kartavyabhayana1@gmail.com>
2024-06-16 12:01:10 -0700

    create login signup forget reset verify apis

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
+const authRoute = require("./Routes/AuthRoute");
+const bodyParser = require('body-parser');
+
 const {MONGO_URL,PORT } = process.env;
+app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
 app.use(
     cors({
@@ -21,6 +27,7 @@ app.get('/api/test',(req,res)=>{
     res.send("Hello World! v2");
 })
 
+app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
