5acde548c8b8d70937f2d9acdbafaf93df13a68c
Kartavya Bhayana <kartavyabhayana1@gmail.com>
2024-07-04 23:55:18 -0700

    feat: modification in yml file and build to production

--- Middlewares/AuthMiddleware.js
+++ Middlewares/AuthMiddleware.js
@@ -1,10 +1,11 @@
 const User = require("../Models/UserModel");
+const Driver = require("../Models/DriverModel");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const userVerification = async (req, res, next) => {
   try {
-    console.log(req.headers,"HHHH")
+    // console.log(req.headers,"HHHH")
     const token = req.headers.authorization;
 
     if (!token) {
@@ -14,11 +15,14 @@ const userVerification = async (req, res, next) => {
     const decodedToken = jwt.verify(token.replace("Bearer ", ""), process.env.TOKEN_KEY); // Replace "Bearer " with an empty string
 
     const user = await User.findById(decodedToken.id);
-
+const driver =await Driver.findById(decodedToken.id)
     if (user) {
       req.user = user;  // Attach the user to the request object
       next();  // Pass control to the next middleware/route handler
-    } else {
+    } else if(driver){
+      req.user = driver;  // Attach the user to the request object
+      next(); 
+    }else {
       return res.status(401).json({ status: false });
     }
   } catch (error) {
