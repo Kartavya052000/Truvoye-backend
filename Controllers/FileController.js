c6af7902e25281a8788b0cf6c3a796b56e40fcbf
Kartavya Bhayana <kartavyabhayana1@gmail.com>
2024-07-31 19:51:58 -0700

    fix:fs system fix for download api

--- Controllers/FileController.js
+++ Controllers/FileController.js
@@ -1,15 +1,28 @@
 const path = require('path');
+const fs = require('fs'); // To check if file exists
 
 
-module.exports.downloadProposal = async (req, res, next) => {
+exports.downloadProposal = (req, res) => {
+    const filePath = path.join(__dirname, '../Uploads/Truvoye-proposal.pdf');
+    console.log('Resolved file path:', filePath); // Make sure this prints the correct path
 
-    const filename = req.params.filename;
-    console.log(filename)
-    const filePath = path.join(__dirname, 'Uploads', filename);
-    res.download(filePath, (err) => {
+
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.error('File does not exist:', err);
+      return res.status(404).send('File not found');
+    }
+
+
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'attachment; filename=Truvoye-project-proposal.pdf');
+
+
+    res.sendFile(filePath, (err) => {
         if (err) {
-            res.status(404).send('File not found');
+          console.error('Error sending file:', err);
+          res.status(500).send('Internal Server Error');
         }
-    });
-
-}
+      });
+  });
+};
